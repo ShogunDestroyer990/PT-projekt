a0814f4e1c8bf275a475a49f85e57c0739748201
E34tf <dawid.korach@student.put.poznan.pl>
2018-03-24 17:30:28 +0100

    Created GUI and some programming patterns.

---
+++ Puzzle Matcher/Puzzle Matcher/ExtensionMethods.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace Puzzle_Matcher
+{
+	public static class ExtensionMethods
+	{
+		public static string ImagePath { get; set; }
+
+		/// <summary>
+		/// Resize the image to the specified width and height.
+		/// </summary>
+		/// <param name="image">Image to resize.</param>
+		/// <param name="width">The width to resize to.</param>
+		/// <param name="height">The height to resize to.</param>
+		/// <returns>The resized image.</returns>
+		public static Bitmap ResizeImage(Bitmap image, int width, int height)
+		{
+			var destImage = new Bitmap(width, height);
+			var destRectangle = new Rectangle(0, 0, width, height);
+
+			destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+			var graphics = Graphics.FromImage(destImage);
+			graphics.CompositingMode = CompositingMode.SourceCopy;
+			graphics.CompositingQuality = CompositingQuality.HighQuality;
+			graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+			graphics.SmoothingMode = SmoothingMode.HighQuality;
+			graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+			var wrapMode = new ImageAttributes();
+			wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+			graphics.DrawImage(image, destRectangle, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+
+			return destImage;
+		}
+
+	}
+}
---
+++ Puzzle Matcher/Puzzle Matcher/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Puzzle_Matcher
+{
+	partial class MainWindow
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+			this.ImageIn = new System.Windows.Forms.PictureBox();
+			this.ProcessImage = new System.Windows.Forms.PictureBox();
+			this.ofd = new System.Windows.Forms.OpenFileDialog();
+			((System.ComponentModel.ISupportInitialize)(this.ImageIn)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.ProcessImage)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// ImageIn
+			// 
+			this.ImageIn.Image = global::Puzzle_Matcher.Properties.Resources.ButtonLoadImage;
+			this.ImageIn.InitialImage = null;
+			this.ImageIn.Location = new System.Drawing.Point(13, 13);
+			this.ImageIn.Name = "ImageIn";
+			this.ImageIn.Size = new System.Drawing.Size(759, 431);
+			this.ImageIn.TabIndex = 0;
+			this.ImageIn.TabStop = false;
+			this.ImageIn.Click += new System.EventHandler(this.ImageIn_Click);
+			// 
+			// ProcessImage
+			// 
+			this.ProcessImage.Image = global::Puzzle_Matcher.Properties.Resources.ButtonProcessImage;
+			this.ProcessImage.InitialImage = null;
+			this.ProcessImage.Location = new System.Drawing.Point(12, 450);
+			this.ProcessImage.Name = "ProcessImage";
+			this.ProcessImage.Size = new System.Drawing.Size(760, 100);
+			this.ProcessImage.TabIndex = 1;
+			this.ProcessImage.TabStop = false;
+			this.ProcessImage.Click += new System.EventHandler(this.ProcessImage_Click);
+			// 
+			// ofd
+			// 
+			this.ofd.DefaultExt = "%HOMEPATH%";
+			this.ofd.FileName = "WybierzZdjecie";
+			this.ofd.Filter = "PNG Image (.png)|*.png|GIF Image (.gif)|*.gif|JPG Image (.jpg) |*.jpg|BITMAP Imag" +
+    "e (.bmp)|*.bmp|All files (.*)|*.*";
+			this.ofd.Title = "Wybierz zdjęcie";
+			// 
+			// MainWindow
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(784, 562);
+			this.Controls.Add(this.ProcessImage);
+			this.Controls.Add(this.ImageIn);
+			this.HelpButton = true;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "MainWindow";
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Okno główne";
+			((System.ComponentModel.ISupportInitialize)(this.ImageIn)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.ProcessImage)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox ImageIn;
+		private System.Windows.Forms.PictureBox ProcessImage;
+		private System.Windows.Forms.OpenFileDialog ofd;
+	}
+}
+
