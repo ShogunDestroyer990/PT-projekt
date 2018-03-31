c277fab6a0adbfbc599cfe4cc0f70c4b9366d11b
ShogunDestroyer990 <adam.godzinski@student.put.poznan.pl>
2018-03-31 17:54:29 +0200

    Add files via upload
    znajduje puzzle i dzieli je na osobne obrazki ale brak wyświetlania

--- ExtensionMethods.cs
+++ ExtensionMethods.cs
@@ -27,6 +27,11 @@ namespace Puzzle_Matcher
 
         public static object ImageIn { get; set; }
 
+        public static void isolatepuzzles()
+        {
+
+        }
+
         public static void findEdges()
         {
             Image<Bgr, Byte> My_Image = new Image<Bgr, Byte>(ImagePath);
@@ -37,48 +42,82 @@ namespace Puzzle_Matcher
 
             CvInvoke.GaussianBlur(gray, gray, new Size(), 5, 5, BorderType.Default);
 
-            Image<Gray, Byte> Imgout =gray;
+            Image<Gray, Byte> Imgout = gray;
 
 
-             CvInvoke.AdaptiveThreshold(gray, Imgout, 250, Emgu.CV.CvEnum.AdaptiveThresholdType.MeanC, ThresholdType.BinaryInv, 39, 4);
+            CvInvoke.AdaptiveThreshold(gray, Imgout, 250, Emgu.CV.CvEnum.AdaptiveThresholdType.MeanC, ThresholdType.BinaryInv, 39, 4);
             //CvInvoke.GaussianBlur(gray, gray, new Size(), 5, 5, BorderType.Default);
-           
-
-
-             Imgout._Dilate(5);
-             Imgout._Erode(4);
 
 
 
-            CvInvoke.FindContours(Imgout, contours, hierarchy, Emgu.CV.CvEnum.RetrType.External, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);           
-            VectorOfVectorOfPoint corected = new VectorOfVectorOfPoint();       
-            double max = 10;
+            Imgout._Dilate(5);
+            Imgout._Erode(4);
 
-           
 
-            for (int i = 0; i < contours.Size; i++) 
+
+            CvInvoke.FindContours(Imgout, contours, hierarchy, Emgu.CV.CvEnum.RetrType.External, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
+            VectorOfVectorOfPoint corected = new VectorOfVectorOfPoint();
+
+
+            double avreage = 0;
+
+
+
+            for (int i = 0; i < contours.Size; i++)
 
             {
+                avreage += CvInvoke.ContourArea(contours[i], false);
+            }
 
-                if (CvInvoke.ContourArea(contours[i],false) > max)
+            avreage = avreage / contours.Size;
+
+            avreage = (int)avreage * 0.85;
+
+            for (int i = 0; i < contours.Size; i++)
+            {
+                if (CvInvoke.ContourArea(contours[i], false) > avreage)
                 {
-
-                    max = CvInvoke.ContourArea(contours[i], false);
-
                     corected.Push(contours[i]);
-
                 }
+            }
+
+
+
+
+            CvInvoke.DrawContours(My_Image, corected, -1, new MCvScalar(255, 0, 0), 10);
+            CvInvoke.PutText(My_Image, "puzzles find:" + corected.Size, new Point(200, 250), FontFace.HersheySimplex, 10, new MCvScalar(255, 0, 0), 2);
+
+
+           // Rectangle[] boundRect = new Rectangle[] { };
+            List<Rectangle> boundRect = new List<Rectangle>();
+
+
+            for (int i = 0; i < corected.Size; i++)
+            {
+                boundRect.Add(CvInvoke.BoundingRectangle(corected[i]));
+
+            }
+            int x = 0;
+            var puzzels = new List<Image<Bgr, byte>>();//lista puzzli
+            var remembre = My_Image; //dla całości
+            foreach (Rectangle r in boundRect)
+            {
+                x++;
+                Image<Bgr, Byte> img = My_Image;
+                img.ROI = r;
+                puzzels.Add(img.Copy());
+                CvInvoke.Rectangle(My_Image, r, new MCvScalar(250, 0, 250), 10, LineType.EightConnected);
+                CvInvoke.PutText(My_Image,x.ToString(), new Point(r.X+r.Width/2, r.Y+r.Height/2), FontFace.HersheySimplex, 8, new MCvScalar(255, 0, 255), 10);
+               
 
             }
 
           
-            CvInvoke.DrawContours(My_Image, contours, -1, new MCvScalar(255, 0, 0),10);
-            //CvInvoke.PutText(My_Image, "puzzles find:" + corected.Size, new Point(200, 250), FontFace.HersheySimplex, 10, new MCvScalar(255, 0, 0),2);
-
-
-            ImageOut2 = My_Image.ToBitmap();
+            ImageOut2 = remembre.ToBitmap();
             ImageOut = Imgout.ToBitmap();
 
+         
+          
 
         }
 
